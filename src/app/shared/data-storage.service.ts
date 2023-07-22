1508dde380ed13cf7e89c0e290a63d68593fc5ff
dileka-itexphere <113968015+dileka-itexphere@users.noreply.github.com>
2023-07-22 11:16:44 +0530

    created add movie reviews and rating function

--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -1,8 +1,10 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Movie } from '../movies/movie.model';
-import { map, tap } from 'rxjs/operators';
+import { catchError, map, tap } from 'rxjs/operators';
 import { TvSeries } from '../tv-series/tv-series.model';
+import { Movie_Review } from '../movies/movie-review.model';
+
 
 @Injectable({
   providedIn: 'root'
@@ -43,12 +45,39 @@ export class DataStorageService {
 
 
     fetchMovieById(id: number) {
-      return this.http.get<Movie>(`https://movie-store-a5b55-default-rtdb.firebaseio.com/movies/${id-1}.json`);
+      return this.http.get<Movie>(`https://movie-store-a5b55-default-rtdb.firebaseio.com/movies/${id-1}.json`)
+      ;
     }
 
     fetchTVSById(id: number) {
       return this.http.get<TvSeries>(`https://movie-store-a5b55-default-rtdb.firebaseio.com/tv/${id-1}.json`);
     }
+
+    submitReview(username: string, comment: string ,rating:Number) {
+      const movieReview = {username, comment ,rating};
+
+      this.http.post<Movie_Review>('https://movie-store-a5b55-default-rtdb.firebaseio.com/movie-reviews.json', movieReview)
+       .subscribe(reponseData =>{
+        console.log(reponseData)
+       }
+
+       )
+    }
+
+
+      fetchReviewsbymovieID(){
+        return this.http.get<Movie_Review[]>(`https://movie-store-a5b55-default-rtdb.firebaseio.com/movie-reviews.json`
+        ).pipe(
+          map(reviews => {
+            return reviews.map(Movie_Review => {
+              // console.log(reviews)
+              return {
+                ...Movie_Review,
+              };
+            });
+          }),
+          )
+      }
 
 }
 
